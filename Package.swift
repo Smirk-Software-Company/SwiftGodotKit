b7ffd3cc9f035cd31f877af7e291ab5e26bedbe9
Josh Bromberg <josh@munchapp.co>
2023-11-22 13:59:45 -0500

    libgodot local

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "SwiftGodotKit",
             dependencies: ["SwiftGodot", "libgodot"]),
-        .binaryTarget(name: "libgodot", url: "https://github.com/Smirk-Software-Company/SmirkGodot/blob/main/libgodot.xcframework.zip", checksum: "8d5d41959ae70785a7606f27afa588aca73f8e30"),
+        .binaryTarget(name: "libgodot", url: "https://github.com/Smirk-Software-Company/SwiftGodotKit/blob/main/libgodot.xcframework.zip", checksum: "76c2ae03151c30f9a7a7fa890ec8dbb1781becd1"),
 //        .binaryTarget (
 //            name: "libgodot",
 //            path: "../libgodot.xcframework"),
