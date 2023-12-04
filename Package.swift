5441bb9c8450e1538d165829ec94a1ab90708233
jbromberg <58891459+jbromberg@users.noreply.github.com>
2023-12-04 14:57:23 -0500

    local packages

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["SwiftGodotKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Smirk-Software-Company/SwiftGodot", branch: "main")
-//        .package(path: "../SwiftGodot")
+        // .package(url: "https://github.com/Smirk-Software-Company/SwiftGodot", branch: "main")
+       .package(path: "../SwiftGodot")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -25,10 +25,10 @@ let package = Package(
         .target(
             name: "SwiftGodotKit",
             dependencies: ["SwiftGodot", "libgodot"]),
-        .binaryTarget(name: "libgodot", url: "https://github.com/Smirk-Software-Company/SwiftGodotKit/raw/76c2ae03151c30f9a7a7fa890ec8dbb1781becd1/libgodot.xcframework.zip", checksum: "16f54f90ae55ed425cfb1e19528b9e0a14f009411c92bd04db504f730b6dac5e"),
-//        .binaryTarget (
-//            name: "libgodot",
-//            path: "../libgodot.xcframework"),
+        // .binaryTarget(name: "libgodot", url: "https://github.com/Smirk-Software-Company/SwiftGodotKit/raw/76c2ae03151c30f9a7a7fa890ec8dbb1781becd1/libgodot.xcframework.zip", checksum: "16f54f90ae55ed425cfb1e19528b9e0a14f009411c92bd04db504f730b6dac5e"),
+       .binaryTarget (
+           name: "libgodot",
+           path: "../libgodot.xcframework"),
         .testTarget(
             name: "SwiftGodotKitTests",
             dependencies: ["SwiftGodotKit"]),
